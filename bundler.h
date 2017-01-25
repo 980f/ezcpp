68fba305d6b27d1f3ba4a6a923d460b403c7b862
andyh <andy.980f@gmail.com>
2017-01-24 22:50:22 -0600

    removed tester, feature works and is also passe

--- bundler.h
+++ bundler.h
@@ -6,15 +6,16 @@ Once the core works we will try to incorporate lamdas.
 */
 template <class Grouped> class Bundler {
   static Bundler *list;//=nullptr;//zero init by nature
+  /** a private link keeps caller from slipping in a null on us and orphaning the tail */
   Bundler *next;
 public:
   Bundler():next(0){
-    //insert into list, presumes that timer service isn't started until all timer objects are constructed.
-    if(list == 0) {//if first to be created
+    //insert into list
+    if(list == nullptr) {//if first to be created
       list = this; //start the list
     } else {
       for(Bundler *scan = list; scan ; scan = scan->next) {
-        if(scan->next == 0) {
+        if(scan->next == nullptr) {
           scan->next = this;
           break;
         }
@@ -22,9 +23,7 @@ public:
     }
   }
 
-//can't destruct until we add some  more linker support:
-// __aeabi_atexit  __dso_handle
-  /** unlink object */
+  /** unlink object, this does NOT free them, freeing these calls this. */
   ~Bundler(){
     if(list==this){
       list=list->next;
@@ -40,21 +39,28 @@ public:
 
   typedef  void (Grouped::*groupedfn)(void);
 
-  /** do a grouped action */
+  /** call a member function on all members of the group */
   static void forAll( groupedfn fn){
     for(Bundler *scan = list; scan ; scan = scan->next) {
       (static_cast<Grouped *>(scan)->*fn)();
     }
   }
 
-  /** do a grouped action */
+  /** for each member of the group call a function that takes a member as an argument. */
   static void forAll(void (*fn)(Grouped &groupee)){
     for(Bundler *scan = list; scan ; scan = scan->next) {
       fn(*scan);
     }
   }
 
+  /** if this bundle is the sole owner of pointers you need some means to destroy the entities */
+  static void freeAll(){
+    while(list){
+      delete list;//this has a side effect of altering list.
+    }
+  }
+
 };
 
-
+/** the following seems a bit suspicious, might generate multiple instances */
 template <typename T> Bundler<T>* Bundler<T>::list( 0 );
