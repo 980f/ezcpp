214943d3861b874806d7831a19e36786d3458f03
andyh <andy.980f@gmail.com>
2025-03-27 12:19:06 -0500

    added utility methods

--- block.h
+++ block.h
@@ -3,4 +3,23 @@
 template <typename ContentType> struct Block {
   const size_t size;
   ContentType &content;
+
+  bool isVacuous() const {
+    return size == 0 || &content == nullptr;
+  }
+
+  bool operator !() const {
+    return isVacuous();
+  }
+  
+  /** @returns this as anonymous block of bytes */
+  Block<uint8_t> raw() {
+    return Block<uint8_t>{size*sizeof(ContentType),reinterpret_cast<uint8_t &>(content)};
+  }
+  
+  /** @returns this as anonymous block of bytes */
+  Block<const uint8_t> c_raw() const {
+    return Block<const uint8_t>{size*sizeof(ContentType),reinterpret_cast<const uint8_t &>(content)};
+  }
+  
 };
