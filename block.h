96ba8034233399d373b9e346573f1e49b080d5b5
andyh <andy.980f@gmail.com>
2025-04-17 21:26:52 -0500

    breaking change to allow for easier inline creation, and also get rid of many '&'s at points of use

--- block.h
+++ block.h
@@ -4,33 +4,44 @@
   for retrofitting old code that took pointer and size as pair */
 
 template<typename ContentType> struct Block {
-  const size_t size;
-  ContentType &content;
-
-  bool isVacuous() const {
-    return (size == 0) || (&content == nullptr);
-  }
-
-  bool operator !() const {
-    return isVacuous();
-  }
-
-  ContentType& operator[](unsigned index){
-    static ContentType hiddenbug;
-    if(index<size){
-      return (&content)[index];
-    } else {
-      return hiddenbug; //reference outside the index results in (potential) bleeding of one bug's value to another bug's reference.
+  //soon: private:
+    size_t size {0};
+    ContentType *content{nullptr};
+  public:
+    size_t quantity() const {
+      return size;
     }
-  } 
+  
+    Block& operator =(Block &&other) {
+      size = other.size;
+      content = other.content;
+      return *this;
+    }
 
-  /** @returns this as anonymous block of bytes */
-  Block <uint8_t> raw() {
-    return Block <uint8_t> {size * sizeof(ContentType), reinterpret_cast <uint8_t &>(content)};
-  }
+    bool isVacuous() const {
+      return (size == 0) || (content == nullptr);
+    }
 
-  /** @returns this as anonymous block of bytes */
-  Block <const uint8_t> c_raw() const {
-    return Block <const uint8_t> {size * sizeof(ContentType), reinterpret_cast <const uint8_t &>(content)};
-  }
+    bool operator !() const {
+      return isVacuous();
+    }
+
+    ContentType& operator[](unsigned index) const {
+      static ContentType hiddenbug{0};
+      if (index < size) {
+        return content[index];
+      } else {
+        return hiddenbug; //reference outside the index results in (potential) bleeding of one bug's value to another bug's reference.
+      }
+    }
+
+    /** @returns this as anonymous block of bytes */
+    Block <uint8_t> raw() {
+      return Block <uint8_t> {size * sizeof(ContentType), reinterpret_cast <uint8_t *>(content)};
+    }
+
+    /** @returns this as anonymous block of bytes */
+    Block <const uint8_t> c_raw() const {
+      return Block <const uint8_t> {size * sizeof(ContentType), reinterpret_cast <const uint8_t *>(content)};
+    }
 };
